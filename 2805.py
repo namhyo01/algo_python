c8f54506afb86ea11c8ff47d0267e66c5cae2c46
namhyo01@naver.com <namhyo01@naver.com>
2022-08-19 11:51:54 +0900

    2805

--- 2805.py
+++ 2805.py
@@ -2,3 +2,19 @@ import sys
 
 N,M = map(int, sys.stdin.readline().split())
 trees = list(map(int, sys.stdin.readline().split()))
+l,r = 0,1000000000
+res = 0
+while l<=r:
+    mid = (l+r)//2
+    tot = 0
+    for t in trees:
+        if(mid<t):
+            tot+=t-mid
+        if(tot>=M):
+            break
+    if(tot>=M):
+        res = mid
+        l = mid+1
+    else:
+        r = mid-1
+print(res)
