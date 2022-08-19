97bd2b4df449e475ab83d0e6a1d17f2c2dbe454c
namhyo01@naver.com <namhyo01@naver.com>
2022-08-19 15:24:00 +0900

    19637

---
+++ 19637.py
@@ -0,0 +1,13 @@
+import sys
+from bisect import *
+
+N,M = map(int,sys.stdin.readline().split())
+title, value = [],[]
+for _ in range(N):
+    t,v = sys.stdin.readline().split()
+    title.append(t)
+    value.append(int(v))
+
+for _ in range(M):
+    power = int(sys.stdin.readline())
+    print(title[bisect_left(value,power)])
